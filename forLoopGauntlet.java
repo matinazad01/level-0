40ba5e14e9486572379fedd0d38ebcb699f2169d
matinazad01 <matin.azad@jointheleague.org>
2017-08-15 18:53:17 -0700

    Add files via upload

---
+++ forLoopGauntlet.java
@@ -0,0 +1,49 @@
+package open;
+
+public class forLoopGauntlet{
+
+	public static void main(String[] args) {
+		
+	
+	
+	
+	
+	for(int hi=2002; hi<=2017; hi++){ 
+		
+	
+		switch(hi){
+		case 2002:
+		System.out.println(hi+"- I was born!");
+		break;
+		case 2003:
+			System.out.println(hi + "- I was " + (hi-2002) + " year old!");
+			break;
+		case 2017:
+			System.out.println(hi+ "- I am " + (hi-2002) + " years old!");
+		break;
+		
+		default:
+			System.out.println(hi + "- I was " + (hi-2002) + " years old!");}
+		}
+		}
+		
+		
+	}
+		
+				
+		
+//	if(hi==1){ 
+//		System.out.print(hi + " and ");
+//	}else if(hi==0){
+//		System.out.print(hi + ".");
+//		
+//	}else{System.out.print(hi + ", ");}
+		
+	
+	
+	
+
+	
+	
+	
+
